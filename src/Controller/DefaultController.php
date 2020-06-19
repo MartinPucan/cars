7a7e91dbfca49f5eefffc6a243d122cf4e4421dd
Martin Pucan <martin.pucan@pilulka.cz>
2020-06-19 14:17:06 +0200

    create simple layout - display all items

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CarRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    private CarRepository $carRepository;
+
+    public function __construct(CarRepository $carRepository)
+    {
+
+        $this->carRepository = $carRepository;
+    }
+
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        $cars = $this->carRepository->findAll();
+
+
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'cars' => $cars,
+        ]);
+    }
+}
---
+++ src/Entity/Car.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CarRepository::class)
+ */
+class Car
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $kilometer;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $registration;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fuelType;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getKilometer(): ?float
+    {
+        return $this->kilometer;
+    }
+
+    public function setKilometer(float $kilometer): self
+    {
+        $this->kilometer = $kilometer;
+
+        return $this;
+    }
+
+    public function getRegistration(): ?int
+    {
+        return $this->registration;
+    }
+
+    public function setRegistration(int $registration): self
+    {
+        $this->registration = $registration;
+
+        return $this;
+    }
+
+    public function getFuelType(): ?string
+    {
+        return $this->fuelType;
+    }
+
+    public function setFuelType(string $fuelType): self
+    {
+        $this->fuelType = $fuelType;
+
+        return $this;
+    }
+}
